b26202c1fd029ebe2f57c8c6af4de5175d989522
bhushan1312-code <bhushanavhad1312@gmail.com>
2025-07-04 00:06:46 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
- let a;
- let date;
- let time;
- const option ={
-weekday:'long', year:'numeric',month:'long',day:'numeric'
-};
- 
+let timezone = "Asia/Kolkata"
+
+
+function updateTime() {
+  const select = document.getElementById("country");
+  timezone = select.value;
+}
 
 setInterval(() => {
-a = new Date();
-time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
-document.getElementById('head').innerHTML = time;
-}, 1000); 
+  const now = new Date();
+  const time = now.toLocaleTimeString('en-US', {
+    timeZone: timezone,
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true
+  });
+
+  document.getElementById("head").innerHTML = time;
+}, 1000);
